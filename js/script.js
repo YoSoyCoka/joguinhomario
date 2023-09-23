92478d311e00e0c6d4ee4205eb6f81be6ccd0a66
YoSoyCoka <gabrielrcoca@outlook.com>
2023-09-23 16:04:54 -0300

    new file:   css/style.css 	new file:   imgs/728a020d5290efa08d853c061307eb31.gif 	new file:   imgs/Mario_Series_Logo.svg.png 	new file:   imgs/Super-Mario-Bros-King-Boo-PNG-File.png 	new file:   imgs/clouds.png 	new file:   imgs/game-over.png 	new file:   imgs/mario.gif 	new file:   imgs/moeda.gif	new file:   imgs/pipe.png 	new file:   index.html 	new file:   js/script.js 	new file:   sounds/Som de moeda do sonic mp3.mp4

---
+++ js/script.js
@@ -0,0 +1,57 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+const moeda = document.querySelector('.moeda');
+const audio = document.querySelector('audio');
+
+const jump = () => {
+    mario.classList.add('jump');
+
+    setTimeout(() => {
+        mario.classList.remove('jump');
+    }, 600);
+}
+
+const loop = setInterval(() => {
+
+    const PipePos = pipe.offsetLeft;
+    const marioPos = +window.getComputedStyle(mario).bottom.replace('px', '');
+    const moedaPos = moeda.offsetLeft;
+    
+    
+    
+        // game over
+    if ((PipePos <= 120) && (marioPos < 80) && (PipePos > 0)) {
+        pipe.style.animation = 'none';
+        pipe.style.left = `${PipePos + 10}px`;
+        
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPos + 10}px`;
+        mario.src = 'imgs/game-over.png';
+        mario.style.width = '75px';
+        mario.style.marginLeft = '60px';
+
+        clearInterval(loop);
+    } 
+
+    if(moedaPos <= 120 && moedaPos > 0 && marioPos < 80){
+        moeda.classList.remove('moeda');
+        void moeda.offsetWidth; 
+        moeda.classList.add('moeda');
+        audio.play();
+
+    } 
+    
+    
+}, 10);
+
+
+
+
+
+
+
+
+
+document.addEventListener('keydown', jump);
+
+
