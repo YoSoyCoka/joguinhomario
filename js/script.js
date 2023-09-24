a755d4cadd9762cb24ebb84923448a2c272e930c
YoSoyCoka <gabrielrcoca@outlook.com>
2023-09-24 01:50:43 -0300

    modified:   css/style.css 	deleted:    imgs/728a020d5290efa08d853c061307eb31.gif 	new file:   imgs/ab67616d0000b273ccb7730680297da990ce7ede-removebg-preview.png 	modified:   index.html 	modified:   js/script.js 	new file:   sounds/gameover.mp4

--- js/script.js
+++ js/script.js
@@ -2,7 +2,10 @@ const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
 const moeda = document.querySelector('.moeda');
 const audio_moeda = document.querySelector('audio');
-
+const audio_fundo = document.getElementById('audio-principal');
+const audio_gameover = document.getElementById('game-over-sound');
+const pontuacao = document.querySelector('.score');
+const logo = document.querySelector('.logo');
 
 
 const jump = () => {
@@ -18,10 +21,9 @@ const loop = setInterval(() => {
     const PipePos = pipe.offsetLeft;
     const marioPos = +window.getComputedStyle(mario).bottom.replace('px', '');
     const moedaPos = moeda.offsetLeft;
-    
-    
-    
-        // game over
+    audio_fundo.play();
+
+    // game over
     if ((PipePos <= 120) && (marioPos < 80) && (PipePos > 0)) {
         pipe.style.animation = 'none';
         pipe.style.left = `${PipePos + 10}px`;
@@ -32,14 +34,31 @@ const loop = setInterval(() => {
         mario.style.width = '75px';
         mario.style.marginLeft = '60px';
 
+        logo.src = 'imgs/ab67616d0000b273ccb7730680297da990ce7ede-removebg-preview.png'
+        logo.style.top = '100px';
+        audio_fundo.pause();
+        audio_gameover.play();
+
+        pontuacao.innerHTML = '0 x';
         clearInterval(loop);
     } 
 
+
+    // pontuação
     if(moedaPos <= 120 && moedaPos > 0 && marioPos > 80){
         moeda.classList.remove('moeda');
         void moeda.offsetWidth; 
         moeda.classList.add('moeda');
         audio_moeda.play();
+        pontuacao.innerHTML = parseInt(pontuacao.innerHTML) + 1 + ' x';
+
+         let ehscorealto = +pontuacao.innerHTML.replace('x', '');
+
+         if (ehscorealto >= 5) {
+            pipe.classList.remove('pipe');
+            void pipe.offsetWidth;
+            pipe.classList.add('pipe-rapido');
+         }
 
     } 
     
@@ -47,5 +66,11 @@ const loop = setInterval(() => {
 }, 10);
 
 document.addEventListener('keydown', jump);
+
+
+function reniciargame() {
+    window.location.reload();
+    window.prompt('Digite seu nome: ');
+}
 
 
