07fc57fc9c8bc9332bf54379f2589660fbe3e646
YoSoyCoka <gabrielrcoca@outlook.com>
2023-09-24 23:11:14 -0300

    modified:   css/style.css 	new file:   imgs/3e477e83c35e2a7a38f19ccdad163faa.gif 	new file:   imgs/logo_coka_jump.png 	new file:   imgs/mario_menu.gif 	new file:   imgs/skins/238739_2545907250.gif 	new file:   imgs/skins/pikachu.gif 	new file:   imgs/skins/tumblr_91f98c78ec93adb00b51640b91149491_04262d0a_500.gif 	new file:   imgs/skins/ursos.gif 	modified:   index.html 	modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@ const audio_fundo = document.getElementById('audio-principal');
 const audio_gameover = document.getElementById('game-over-sound');
 const pontuacao = document.querySelector('.score');
 const logo = document.querySelector('.logo');
+const jogo = document.querySelector('.borda_principal_game');
+const tela_inicial = document.querySelector('.tela_inicial');
 
 
 const jump = () => {
@@ -16,54 +18,58 @@ const jump = () => {
     }, 600);
 }
 
-const loop = setInterval(() => {
+function iniciarJogo(){
+    jogo.style.display = 'block';
+    tela_inicial.style.display = 'none';
+    const loop = setInterval(() => {
 
-    const PipePos = pipe.offsetLeft;
-    const marioPos = +window.getComputedStyle(mario).bottom.replace('px', '');
-    const moedaPos = moeda.offsetLeft;
-    audio_fundo.play();
-
-    // game over
-    if ((PipePos <= 120) && (marioPos < 80) && (PipePos > 0)) {
-        pipe.style.animation = 'none';
-        pipe.style.left = `${PipePos + 10}px`;
+        const PipePos = pipe.offsetLeft;
+        const marioPos = +window.getComputedStyle(mario).bottom.replace('px', '');
+        const moedaPos = moeda.offsetLeft;
+        audio_fundo.play();
+    
+        // game over
+        if ((PipePos <= 120) && (marioPos < 80) && (PipePos > 0)) {
+            pipe.style.animation = 'none';
+            pipe.style.left = `${PipePos + 10}px`;
+            
+            mario.style.animation = 'none';
+            mario.style.bottom = `${marioPos + 10}px`;
+            mario.src = 'imgs/game-over.png';
+            mario.style.width = '75px';
+            mario.style.marginLeft = '60px';
+    
+            logo.src = 'imgs/ab67616d0000b273ccb7730680297da990ce7ede-removebg-preview.png'
+            logo.style.top = '100px';
+            audio_fundo.pause();
+            audio_gameover.play();
+    
+            pontuacao.innerHTML = '0 x';
+            clearInterval(loop);
+        } 
+    
+    
+        // pontuação
+        if(moedaPos <= 120 && moedaPos > 0 && marioPos > 80){
+            moeda.classList.remove('moeda');
+            void moeda.offsetWidth; 
+            moeda.classList.add('moeda');
+            audio_moeda.play();
+            pontuacao.innerHTML = parseInt(pontuacao.innerHTML) + 1 + ' x';
+    
+             let ehscorealto = +pontuacao.innerHTML.replace('x', '');
+    
+             if (ehscorealto >= 3) {
+                pipe.classList.remove('pipe');
+                void pipe.offsetWidth;
+                pipe.classList.add('pipe-rapido');
+             }
+    
+        } 
         
-        mario.style.animation = 'none';
-        mario.style.bottom = `${marioPos + 10}px`;
-        mario.src = 'imgs/game-over.png';
-        mario.style.width = '75px';
-        mario.style.marginLeft = '60px';
-
-        logo.src = 'imgs/ab67616d0000b273ccb7730680297da990ce7ede-removebg-preview.png'
-        logo.style.top = '100px';
-        audio_fundo.pause();
-        audio_gameover.play();
-
-        pontuacao.innerHTML = '0 x';
-        clearInterval(loop);
-    } 
-
-
-    // pontuação
-    if(moedaPos <= 120 && moedaPos > 0 && marioPos > 80){
-        moeda.classList.remove('moeda');
-        void moeda.offsetWidth; 
-        moeda.classList.add('moeda');
-        audio_moeda.play();
-        pontuacao.innerHTML = parseInt(pontuacao.innerHTML) + 1 + ' x';
-
-         let ehscorealto = +pontuacao.innerHTML.replace('x', '');
-
-         if (ehscorealto >= 3) {
-            pipe.classList.remove('pipe');
-            void pipe.offsetWidth;
-            pipe.classList.add('pipe-rapido');
-         }
-
-    } 
-    
-    
-}, 10);
+        
+    }, 10);
+}
 
 document.addEventListener('keydown', jump);
 
